cd42431a34ae5b01006c55f9d1188b4ba1096f52
lordvins226 <kevineilboudo54@gmail.com>
2020-10-13 13:53:39 +0000

    :hatching_chick: initial commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:dart_api/dart_api.dart';
+
+Future main() async {
+  final app = Application<DartApiChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 8888;
+
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ lib/dart_api.dart
@@ -0,0 +1,11 @@
+/// dart_api
+///
+/// A Aqueduct web server.
+library dart_api;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
